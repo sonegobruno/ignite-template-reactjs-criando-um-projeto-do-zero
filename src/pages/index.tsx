b80bb22258c1481c736ac0482f35103cd4f8f377
Bruno Sonego <sonego.bruno@gmail.com>
2021-05-03 15:48:27 -0300

    Adicionado correçoes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi'
 
 interface Post {
   uid?: string;
@@ -24,9 +26,66 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+        <Head>
+          <title>Home | Ig.news</title>
+        </Head>
+        <main className={styles.container}>
+          <div className={styles.logo}>
+            <img src="/images/logo.svg" alt="logo"/>
+          </div>
+          <ul className={styles.posts}>
+            <li>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização</p>
+              <div>
+                <div>
+                  <FiCalendar />
+                  19 abr 2021
+                </div>
+                <div>
+                  <FiUser />
+                  Danilo Vieira
+                </div>
+              </div>
+            </li>
+            <li>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização</p>
+              <div>
+                <div>
+                  <FiCalendar />
+                  19 abr 2021
+                </div>
+                <div>
+                  <FiUser />
+                  Danilo Vieira
+                </div>
+              </div>
+            </li>
+            <li>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização</p>
+              <div>
+                <div>
+                  <FiCalendar />
+                  19 abr 2021
+                </div>
+                <div>
+                  <FiUser />
+                  Danilo Vieira
+                </div>
+              </div>
+            </li>
+          </ul>
+        </main>
+
+
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
